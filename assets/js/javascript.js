2e3bf83048138b9fe2764692d5bbcaeb9de73b81
Cayean <collinayala@gmail.com>
2022-08-25 20:18:14 -0500

    created array of game genres and for loop to

---
+++ assets/js/javascript.js
@@ -0,0 +1,39 @@
+var startForm = document.querySelector(".container");
+var genresArrayEl = [
+    "Single Player",
+    "Multi-player",
+    "First Person Shooter",
+    "Action",
+    "Adventure",
+    "Racing",
+    "co-operative",
+    "Third-person",
+    "Sandbox",
+    "Real-time strategy",
+    "MOBA",
+    "Role-playing",
+    "Sports",
+    "Simulation",
+    "Horror",
+    "Survival"
+]
+
+for (let i = 0; i < genresArrayEl.length; i++) {
+    genresArrayEl + genresArrayEl[i] + 1;
+    
+}
+console.log(genresArrayEl)
+// The user is presented with the option to take a brief survey on what games they would like to play
+
+var testForm = document.createElement("form");
+
+    
+// The user starts the quiz
+
+// The different catagories are added to a quiz container
+
+// The user selects genres that they are interested in
+
+// all of the users selections are pooled
+
+// the pooled results are added to a for loop
