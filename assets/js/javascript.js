da200867de45fbb53c5f2fc43425a13d279a3541
Daniela Lopez <dlope0831@gmail.com>
2022-08-25 21:34:46 -0500

    created cheapshark api

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,21 +1,6 @@
-var getGamePrice = function() {
-    var priceApi ="https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15";
-    fetch(priceApi).then(function(response) {
-        if (response.ok) {
-            console.log(response);
-            response.json().then(function(data) {
-                console.log(data);
-            })
-        }
-    }
-    )
-};
-// getGamePrice(priceData);
-// var apiUrl = "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15";
-// fetch(apiUrl)
-// .then(data => {
-// return data.json();
-// })
-// .then(data => {
-//     console.log(data.title);
-// });
+fetch('https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=50&sortBy=release')
+    .then(result => result.json())
+    .then(data => {
+        console.log(data);
+    });
+    
