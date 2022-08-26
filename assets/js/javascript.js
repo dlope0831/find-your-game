f96cc3ead8826741cadec1a4898ea30d798fabdb
Cayean <collinayala@gmail.com>
2022-08-26 02:14:38 -0500

    refactoring loop to add genre buttons to container

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,5 +1,6 @@
 var startForm = document.querySelector(".container");
-var genreIdCounter = 0;
+var startSurvery = document.querySelector(".btn-start")
+var dataBtnId = 0;
 
 var genresArrayEl = [
     "Single Player",
@@ -8,7 +9,6 @@ var genresArrayEl = [
     "Action",
     "Adventure",
     "Racing",
-    "co-operative",
     "Third-person",
     "Sandbox",
     "Real-time strategy",
@@ -20,21 +20,42 @@ var genresArrayEl = [
     "Survival"
 ]
 
-for (var i = 0; i < genresArrayEl.length; i++) {
-    genresArrayEl + genresArrayEl[i] + 1;
-    
-}
-
-
-console.log(genresArrayEl)
+// console.log(genresArrayEl)
 // The user is presented with the option to take a brief survey on what games they would like to play
-    
+
 // The user starts the quiz
+var startSurvery = document.createElement("button");
+startSurvery.innerHTML = "Start survey"
+startSurvery.className = "btn btn-start";
+
+startForm.appendChild(startSurvery);
+
+// array is iterated and an id assigned to each of the array items
+var createGenreBtn = function(genreBtn) {
+    
+    for (var i = 0; i < genresArrayEl.length; i++) {
+        var genreBtn = genresArrayEl[i];
+        console.log(genreBtn)
+        
+        
+        var buttonEl = document.createElement("button");
+        buttonEl.className = "btn";
+        buttonEl.setAttribute("data-btn-id", dataBtnId);
+        buttonEl.innerHTML = genreBtn;
+
+    }
+    
+    
+    
+    startForm.appendChild(buttonEl)
+}
 
 // The different catagories are added to a quiz container
 
+// console.log(genresArrayEl);
 // The user selects genres that they are interested in
 
 // all of the users selections are pooled
 
 // the pooled results are added to a for loop
+startSurvery.addEventListener("click", createGenreBtn)
